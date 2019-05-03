4f567346f5f0b8c6dbe035658886f132554afa43
FelipeBockMagagnin <felipe.bock@hotmail.com>
2019-05-03 13:43:54 -0300

    0.61.1

---
+++ Caroline/Assets/Sprites/MiniGame/ShipControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipControl : MonoBehaviour
+{
+    public float vel;
+
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0,vel*Time.deltaTime,0);
+        } 
+        else if(Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0,-vel*Time.deltaTime,0);
+        }
+    }
+}
---
+++ Caroline/Assets/Sprites/MiniGame/SpawnManagerMinigame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerMinigame : MonoBehaviour
+{
+    public GameObject rock;    
+    public float xPos, maxY, minY;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        StartCoroutine(spawnRock());
+    }
+
+    IEnumerator spawnRock()
+    {
+        Instantiate(rock, new Vector3(xPos, Random.Range(minY, maxY), 0), Quaternion.identity);
+        yield return new WaitForSeconds(0.2f);
+        StartCoroutine(spawnRock());
+    }
+}
